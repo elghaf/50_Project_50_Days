619a574991831f364ee599b3aed94592bcf652a8
AYOUB-EL <52068209+bbouya@users.noreply.github.com>
2022-04-23 23:57:01 +0000

    files

--- trello-clone/src/styles.ts
+++ trello-clone/src/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 export const AppContainer = styled.div`
 align-items: flex-start;
@@ -9,6 +9,7 @@ height: 100%;
 padding: 20px;
 width: 100%;
 `
+
 
 export const ColumnContainer = styled.div`
     backgrond-color: #ebecf0;
@@ -32,5 +33,5 @@ export const CardContainer = styled.div`
     padding: 0.5rem 1rem;
     max-width: 300px;
     box-shadow: #091e4240 0px 1px 0px 0px;
-    
+
 `
